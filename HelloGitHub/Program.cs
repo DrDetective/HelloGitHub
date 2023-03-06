ce2c1b97d3fd7ea49781eb0303e00aa080d1fcc6
stepan.bast <stepan.bast@VT1-01>
2023-03-06 10:52:22 +0100

    Přidejte soubory projektu.

---
+++ HelloGitHub/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] fun = { "Basket", "Anime a Manga", "Horror hry", "Skateboard", "Celkově gaming", };
+Console.WriteLine("Aoj, jsem Štěpán Bast, chodím na SŠ Educhem a bavá mě: ");
+
+foreach (var item in fun)
+{
+    Console.WriteLine(item);
+}
