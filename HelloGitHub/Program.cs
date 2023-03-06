dcf89881da5335000b79bc8cb1ea8a458e1b158f
Štěpán Bast <stepan.bast@educhem.cz>
2023-03-06 10:56:14 +0100

    Added color of console

--- HelloGitHub/Program.cs
+++ HelloGitHub/Program.cs
@@ -1,6 +1,6 @@
 ﻿string[] fun = { "Basket", "Anime a Manga", "Horror hry", "Skateboard", "Celkově gaming", };
 Console.WriteLine("Aoj, jsem Štěpán Bast, chodím na SŠ Educhem a bavá mě: ");
-
+Console.ForegroundColor= ConsoleColor.Green;
 foreach (var item in fun)
 {
     Console.WriteLine(item);
